80c2ad56730f31a2a5cef40c15e53d735941869b
Arron <arronzhang@me.com>
2023-09-14 22:50:13 +0800

    Init ui

---
+++ ui/src/wallet.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './wallet.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import Setting from './Setting.vue'
+
+createApp(Setting).mount('#app')
---
+++ ui/vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  server: {
+    port: 8081,
+  },
+  build: {
+    sourcemap: false,
+    //outDir: '../public',
+    rollupOptions: {
+      input: {
+        //main: resolve(__dirname, 'index.html'),
+        wallet: resolve(__dirname, 'wallet.html'),
+      },
+    },
+  },
+})
